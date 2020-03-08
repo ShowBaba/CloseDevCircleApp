9f9094547f935c980cb89802c2aad166d1466ccb
ShowBaba <samwise858@gmail.com>
2020-03-08 23:14:02 +0100

    i Initial code commit

---
+++ app/src/main/java/com/samuel/devcircles/GroupChat.java
@@ -0,0 +1,14 @@
+package com.samuel.devcircles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+
+    }
+}
